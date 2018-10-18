65e2fab87bfd1916ef5e43e7c5739b5f3241ba61
Jeff Britts <jbritts@adobe.com>
2018-10-18 13:39:16 -0400

    add transferTo variable

--- Model/MoveInventoryFromDefault.php
+++ Model/MoveInventoryFromDefault.php
@@ -9,8 +9,7 @@ use Magento\Framework\App\ResourceConnection;
 class MoveInventoryFromDefault
 {
 
-    protected $defaultSource = 'us_warehouse';
-       /** @var ResourceConnection  */
+    /** @var ResourceConnection  */
     protected $resourceConnection;
 
     public function __construct(ResourceConnection $resourceConnection)
@@ -18,11 +17,11 @@ class MoveInventoryFromDefault
        $this->resourceConnection = $resourceConnection;
     }
 
-    public function transfer(){
+    public function transfer(string $transferTo){
 
         $connection = $this->resourceConnection->getConnection();
         $tableName = $connection->getTableName('inventory_source_item');
-        $sql = "update " . $tableName . " set source_code = '".$this->defaultSource."' where source_code = 'default'";
+        $sql = "update " . $tableName . " set source_code = '".$transferTo."' where source_code = 'default'";
         $connection->query($sql);
     }
 
